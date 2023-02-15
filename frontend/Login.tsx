338818ec8fa7f9b69734df9c20c2bb17b352ed31
svyslf <saveyourselfh@gmail.com>
2023-02-15 21:36:18 +0000

    Removid useRef, added Registration page validation

--- frontend/Login.tsx
+++ frontend/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect, useContext } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import logo from "./logo.svg";
 
@@ -9,20 +9,15 @@ axios.defaults.xsrfCookieName = "csrftoken"
 
 
 export default function Login() {
-  const userReference = useRef<HTMLDivElement>(null); // I'm using useRef here to hold state reference that is not needed to render
-  const errorReference = useRef<HTMLDivElement>(null); 
 
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
   const [err, setErr] = useState('');
   const [success, setSuccess] = useState(false);
 
-  if(userReference.current){
-  useEffect(() => {userReference.current!.focus()}, []); // the ! is to avoid a "possible null" error 
-  }
 
   useEffect(() => {setErr(''), [user, password] }); // clear error msg if user starts typing
- 
+
   const handlesubmit = async (e) => { // this function sends form data to /api/login 
     e.preventDefault();
 
@@ -64,14 +59,15 @@ export default function Login() {
               <h2 className="mt-6 text-center text-5xl font-bold tracking-tight text-gray-900 sm:text-7xl">
                 Log in
               </h2>
-            </div><div ref={errorReference} className={err ? "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" : "visibility: hidden"} role="alert">
+            </div>
+            <div className={err ? "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" : "visibility: hidden"} role="alert">
                 <strong className="font-bold"> Error! </strong>
                 <span className="block sm:inline"> Incorrect username or password. </span>
                 <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
                   <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
                 </span>
-              </div><form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handlesubmit}>
-
+              </div>
+              <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handlesubmit}>
                 <input type="hidden" name="remember" defaultValue="true" />
                 <div className="-space-y-px rounded-md shadow-sm">
                   <div>
--- frontend/Register.tsx
+++ frontend/Register.tsx
@@ -1,14 +1,52 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import logo from "./logo.svg";
 
+
+const USER_REGEX = new RegExp('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'); //email constraint
+const PWD_REGEX = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})');
+
 function Register(){
- 
     const [user, setUser] = useState('');
+    const [validUser, setValidUser] = useState(false);
+    
     const [password, setPassword] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    
+
     const [confirmPwd, setConfirmPwd] = useState('');
+    const [validCPwd, setvalidCpwd] = useState(false);
+   
     const [err, setErr] = useState('');
     const [success, setSuccess] = useState(false);
+
+
+    useEffect(()=> {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidUser(result);
+    }, [user])
+
+    useEffect(()=> {
+        const result = PWD_REGEX.test(password);
+        console.log(result);
+        console.log(password);
+        setValidPwd(result);
+        const validP = password === confirmPwd;
+        setvalidCpwd(validP)
+        console.log(validP);
+    }, [password, confirmPwd])
+
+    useEffect(() => {setErr(''), [user, password, confirmPwd] }); // clear error msg if user starts typing
+
+    let scheme = false;                         //this is for alternating button styles on disable
+    const btnTheme= ()=> {   
+    if (!validUser|| !validPwd || !validCPwd) {
+        scheme = true; 
+    }
+    return scheme;
+    }
 
     const handlesubmit = async (e) => { // this function sends form data to /api/login 
         e.preventDefault();
@@ -46,7 +84,14 @@ function Register(){
                 <h2 className="mt-6 text-center text-5xl font-bold tracking-tight text-gray-900 sm:text-7xl">
                     Register
                 </h2>
-            
+
+                <div className={err ? "bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" : "visibility: hidden"} role="alert">
+                    <strong className="font-bold"> Error! </strong>
+                    <span className="block sm:inline"> {err} </span>
+                    <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                    </span>
+                </div>
                 <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handlesubmit}>
                     <input type="hidden" name="remember" defaultValue="true" />
                     <div className="-space-y-px rounded-md shadow-sm">
@@ -65,7 +110,12 @@ function Register(){
                             onChange={(e) => setUser(e.target.value)}
                             value={user}
                             />
+                            <div className={!validUser && user ? "bg-lime-100 border-t border-b border-lime-900 text-stone-700 px-4 py-3":"visibility: hidden"} role="alert">
+                                <p className="font-bold">Info</p>
+                                <p className="text-sm">Email address must be a valid Exeter University email! </p>
+                            </div>
                         </div>
+                        
                         <div>
                             <label htmlFor="password" className="sr-only">
                             Password
@@ -81,6 +131,11 @@ function Register(){
                             onChange={(e) => setPassword(e.target.value)}
                             value={password}
                             />
+                            <div className={!validPwd && password ? "bg-lime-100 border-t border-b border-lime-900 text-stone-700 px-4 py-3":"visibility: hidden"} role="alert">
+                                <p className="font-bold">Info</p>
+                                <p className="text-sm"> Your password must have 8 or more characters. 
+                                    It must contain numbers and lowercase, uppercase, and special characters.  </p>
+                            </div>
                         </div>
                         <div>
                             <label htmlFor="password" className="sr-only">
@@ -97,13 +152,16 @@ function Register(){
                             onChange={(e) => setConfirmPwd(e.target.value)}
                             value={confirmPwd}
                             />
+                            <div className={!validCPwd && confirmPwd && password ? "bg-lime-100 border-t border-b border-lime-900 text-stone-700 px-4 py-3":"visibility: hidden"} role="alert">
+                                <p className="font-bold">Info</p>
+                                <p className="text-sm"> Your passwords must match. </p>
+                            </div>
                         </div>
                     </div>
                     <div>
-                        <button
-                            type="submit"
-                            className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-800 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-offset-2"
-                        >
+                        <button disabled = {!validUser|| !validPwd || !validCPwd ? true: false} 
+                            className= { btnTheme() ?" group relative flex w-full justify-center rounded-md border border-transparent bg-green-800 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-offset-2 opacity-50 cursor-not-allowed" : "group relative flex w-full justify-center rounded-md border border-transparent bg-green-800 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-offset-2"
+    }>
                             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                             </span>
                             Register
