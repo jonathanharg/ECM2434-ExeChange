0c4db7f4ca183130708d398296101e942bd8bd13
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-02-16 16:47:04 +0000

    Fixed tailwind for subfolders

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./index.html", "./*.{js,ts,jsx,tsx}", "./*/*.{js,ts,jsx,tsx}"],
+  content: ["./frontend/index.html", "./frontend/*.{js,ts,jsx,tsx}", "./frontend/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
