3a730d04329dcd371deec68f3def888a0465e6c9
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-02-16 16:49:39 +0000

    formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./frontend/index.html", "./frontend/*.{js,ts,jsx,tsx}", "./frontend/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./frontend/index.html",
+    "./frontend/*.{js,ts,jsx,tsx}",
+    "./frontend/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
