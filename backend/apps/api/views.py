323465581515c8b1336a1f63e6a72a2926b06810
Harry Findlay <harryfindlay@outlook.com>
2023-02-16 10:14:17 +0000

    Resolving pull issues

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -1,9 +1,9 @@
-import typing
+from django.http import HttpRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 @api_view(["GET"])
-def status(request: typing.Any) -> Response:
+def status(request: HttpRequest) -> Response:
     data = {"status": "OK", "message": "Coming soon..."}
     return Response(data)
