8eb148a183d764f2c3831ee565ff9d93dc13db57
augustijn <av407@exeter.ac.uk>
2023-02-19 15:48:16 +0000

    Nested routes - Adding a navbar

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -3,18 +3,27 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Login from "./Login";
+import Navbar from "./Navbar";
 
 import "./index.css";
 
+
+//for using nested routes check out: https://reactrouter.com/en/main/start/tutorial
+//info is under the   " nested routes " title, you pretty much need to use an <outlet> to mark 
+//where in the parent route you want the children to render
 const router = createBrowserRouter([
-  {
+  { 
     path: "/",
-    element: <App />,
-    errorElement: <App />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
+    element: <Navbar />,
+    children:[
+      {path: "/login/:login",
+      element: <Login />,},
+      {
+        path: "/test/:test",
+        element: <App />,
+        errorElement: <App />,
+      },
+    ]
   },
 ]);
 
