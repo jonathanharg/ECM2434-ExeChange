05a582310d0fa437769a4baceb8eedb9e0a0354c
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-02-21 15:16:00 +0000

    Removed some sections

--- frontend/Navbar.tsx
+++ frontend/Navbar.tsx
@@ -10,8 +10,6 @@ import {
   ArrowPathIcon,
 } from "@heroicons/react/24/outline";
 import { Link, Outlet } from "react-router-dom";
-// import logo from "./logo.svg";
-// import Profile from "./abstract-user-flat-1.svg";
 
 const navigation = {
   categories: [
@@ -96,7 +94,7 @@ export default function Example() {
   return (
     <>
       <div className="bg-white">
-        {/* "Mobile menu"    ---    (not working as it should ATM)*/}
+        {/* Mobile Menu */}
         <Transition.Root show={open} as={Fragment}>
           <Dialog
             as="div"
@@ -138,91 +136,6 @@ export default function Example() {
                   </div>
 
                   {/* Links */}
-                  <Tab.Group as="div" className="mt-2">
-                    <div className="border-b border-gray-200">
-                      <Tab.List className="-mb-px flex space-x-8 px-4">
-                        {navigation.categories.map((category) => (
-                          <Tab
-                            key={category.name}
-                            className={({ selected }) =>
-                              classNames(
-                                selected
-                                  ? "border-green-800 text-green-600"
-                                  : "border-transparent text-gray-900",
-                                "flex-1 whitespace-nowrap border-b-2 py-4 px-1 text-base font-medium"
-                              )
-                            }
-                          >
-                            {category.name}
-                          </Tab>
-                        ))}
-                      </Tab.List>
-                    </div>
-                    <Tab.Panels as={Fragment}>
-                      {navigation.categories.map((category) => (
-                        <Tab.Panel
-                          key={category.name}
-                          className="space-y-10 px-4 pt-10 pb-8"
-                        >
-                          <div className="grid grid-cols-2 gap-x-4">
-                            {category.featured.map((item) => (
-                              <div
-                                key={item.name}
-                                className="group relative text-sm"
-                              >
-                                <div className="aspect-w-1 aspect-h-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                  <img
-                                    src={item.imageSrc}
-                                    alt={item.imageAlt}
-                                    className="object-cover object-center"
-                                  />
-                                </div>
-                                <a
-                                  href={item.href}
-                                  className="mt-6 block font-medium text-gray-900"
-                                >
-                                  <span
-                                    className="absolute inset-0 z-10"
-                                    aria-hidden="true"
-                                  />
-                                  {item.name}
-                                </a>
-                                <p aria-hidden="true" className="mt-1">
-                                  Shop now
-                                </p>
-                              </div>
-                            ))}
-                          </div>
-                          {category.sections.map((section) => (
-                            <div key={section.name}>
-                              <p
-                                id={`${category.id}-${section.id}-heading-mobile`}
-                                className="font-medium text-gray-900"
-                              >
-                                {section.name}
-                              </p>
-                              <ul
-                                role="list"
-                                aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                                className="mt-6 flex flex-col space-y-6"
-                              >
-                                {section.items.map((item) => (
-                                  <li key={item.name} className="flow-root">
-                                    <a
-                                      href={item.href}
-                                      className="-m-2 block p-2 text-gray-500"
-                                    >
-                                      {item.name}
-                                    </a>
-                                  </li>
-                                ))}
-                              </ul>
-                            </div>
-                          ))}
-                        </Tab.Panel>
-                      ))}
-                    </Tab.Panels>
-                  </Tab.Group>
 
                   <div className="space-y-6 border-t border-gray-200 py-6 px-4">
                     {navigation.pages.map((page) => (
@@ -258,11 +171,6 @@ export default function Example() {
 
                   <div className="border-t border-gray-200 py-6 px-4">
                     <a href="#" className="-m-2 flex items-center p-2">
-                      {/* <img
-                      src={Profile}
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
-                    /> */}
                       <UserCircleIcon className="block h-auto w-5 flex-shrink-0" />
                       <span className="ml-3 block text-base font-medium text-gray-900">
                         Profile
