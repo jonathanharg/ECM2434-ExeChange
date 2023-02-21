c5a7664ac507358444e1efe941a80a2901cfb690
Harry Findlay <harryfindlay@outlook.com>
2023-02-21 13:10:20 +0000

    IntegrityError exception handling

--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.http import HttpRequest
+from django.db.utils import IntegrityError
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -81,25 +82,31 @@ def register(request: HttpRequest) -> Response:
     user_confirm_password = request.data["confirmPwd"]
 
     if user_password == user_confirm_password:
-        # TODO: Add try except
+       
+        try:
+            # Generate new user and add to User database using django.auth User model
+            new_user = User.objects.create_user(
+            username=email_address, email=email_address, password=user_password)
+            new_user.save()
 
-        #Generate new user and add to User database using django.auth User model 
-        new_user = User.objects.create_user(username=email_address, email=email_address, password=user_password)
-        new_user.save()
+            print("USER ADDED TO DATABASE!")
 
-        print("USER ADDED TO DATABASE!")
+            # Generating new JWT token for registered user, this means that they do not need to log in after registering
+            token = gen_token(new_user)
 
-        #Generating new JWT token for registered user, this means that they do not need to log in after registering
-        token = gen_token(new_user)
-
-        data = {
-            "status": "OK",
-            "message": "User authentication excepted",
-            "access": str(token.access_token),
-            "refresh": str(token),
-        }
+            data = {
+                "status": "OK",
+                "message": "User authentication excepted",
+                "access": str(token.access_token),
+                "refresh": str(token),
+            }
+        except IntegrityError as e:
+            data = {
+                "status": "UNIQUE_ERROR",
+                "message": "User already signed up!"
+            }
     else:
-        data = {"status": "BAD", "message": "Password and confirm do not match"}
+        data = {"status": "CREDENTIAL_ERROR", "message": "Password and confirm do not match"}
 
     
 
