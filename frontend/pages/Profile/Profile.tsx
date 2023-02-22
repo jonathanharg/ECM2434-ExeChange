d15f5856af148a70cfa1ab2c0427d09e0566cd5d
Maddie Hansford <madelene.hansford@gmail.com>
2023-02-22 15:30:09 +0000

    profile rough draft

--- frontend/pages/Profile/Profile.tsx
+++ frontend/pages/Profile/Profile.tsx
@@ -3,31 +3,70 @@ import React from "react";
 function Profile(){
 
     return(
-        <div className="max-w-md p-8 sm:flex sm:space-x-6 dark:bg-gray-900 dark:text-gray-100">
-            <div className="flex-shrink-0 w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
-                <img src="https://source.unsplash.com/100x100/?portrait?1" alt="" className="object-cover object-center w-full h-full rounded dark:bg-gray-500" />
-            </div>
-            <div className="flex flex-col space-y-4">
+        <><div className="min-h-screen flex flex-col max-w-md mx-auto bg-white opacity-100 font-poppins px-4 bg-no-repeat bg-cover bg-center">
+            <div className="flex justify-between px-1 pt-4 items-center">
                 <div>
-                    <h2 className="text-2xl font-semibold">Leroy Jenkins</h2>
-                    <span className="text-sm dark:text-gray-400">General manager</span>
-                </div>
-                <div className="space-y-1">
-                    <span className="flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Email address" className="w-4 h-4">
-                            <path fill="currentColor" d="M274.6,25.623a32.006,32.006,0,0,0-37.2,0L16,183.766V496H496V183.766ZM464,402.693,339.97,322.96,464,226.492ZM256,51.662,454.429,193.4,311.434,304.615,256,268.979l-55.434,35.636L57.571,193.4ZM48,226.492,172.03,322.96,48,402.693ZM464,464H48V440.735L256,307.021,464,440.735Z"></path>
-                        </svg>
-                        <span className="dark:text-gray-400">leroy.jenkins@company.com</span>
-                    </span>
-                    <span className="flex items-center space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="Phonenumber" className="w-4 h-4">
-                            <path fill="currentColor" d="M449.366,89.648l-.685-.428L362.088,46.559,268.625,171.176l43,57.337a88.529,88.529,0,0,1-83.115,83.114l-57.336-43L46.558,362.088l42.306,85.869.356.725.429.684a25.085,25.085,0,0,0,21.393,11.857h22.344A327.836,327.836,0,0,0,461.222,133.386V111.041A25.084,25.084,0,0,0,449.366,89.648Zm-20.144,43.738c0,163.125-132.712,295.837-295.836,295.837h-18.08L87,371.76l84.18-63.135,46.867,35.149h5.333a120.535,120.535,0,0,0,120.4-120.4v-5.333l-35.149-46.866L371.759,87l57.463,28.311Z"></path>
-                        </svg>
-                        <span className="dark:text-gray-400">+25 381 77 983</span>
-                    </span>
+                    <p className="font-semibold">My Profile</p>
                 </div>
             </div>
-        </div>
+            <div className="flex items-center px-4 pt-12 justify-between">
+                <div className="w-24 h-24 bg-blue-600 flex items-center rounded-full">
+                    <img className="h-20 w-20 mx-auto" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png">
+                    </img></div>
+                <div className="w-9/12 flex items-center">
+                    <div className="w-10/12 flex flex-col leading-none pl-4">
+                        <p className="text-2xl font-bold">David</p>
+                        <p className="text-sm pt-1 font-light text-gray-700">Level 5</p>
+                    </div>
+                    <div className="w-2/12">
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-700" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M9.243 19H21v2H3v-4.243l9.9-9.9 4.242 4.244L9.242 19zm5.07-13.556l2.122-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z" /></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="pt-12 px-4 w-full flex flex-col">
+                <p className="font-semibold text-gray-600">My Achievements</p>
+                <div className="flex w-full pt-2 space-x-2">
+                    <button className="bg-gray-800 w-32 rounded-full px-4 py-2 font-ligth text-white flex">5 days in a row</button>
+                    <button className="bg-green-800 w-32 rounded-full px-4 py-2 font-ligth text-white flex">Halloween trader</button>
+                    <button className="bg-red-800 w-32 rounded-full px-4 py-2 font-ligth text-white flex">5+ trades!</button>
+                </div>
+            </div>
+            <div className="pt-12 px-4 w-full flex flex-col">
+                <p className="font-semibold text-gray-600">Location Badges</p>
+                <div className="flex flex-col w-full pt-2 space-y-2">
+                    <div className="flex w-full h-12">
+                        <div className="w-2/12 h-full">
+                            <div className="h-12 w-12 bg-green-600 rounded-full flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto text-white" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M2 9h19a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V9zm1-6h15v4H2V4a1 1 0 0 1 1-1zm12 11v2h3v-2h-3z" /></svg>
+                            </div>
+                        </div>
+                        <div className="w-6/12 h-full flex items-start">
+                            <p className="my-auto text-lg font-semibold">Lafrowda</p>
+                        </div>
+                        <div className="w-4/12 h-full flex justify-end items-end">
+                            <button className="flex bg-red-600 rounded-md my-auto px-5 py-1 float-right text-white font-medium">3 Trades</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col w-full pt-2 space-y-2">
+                    <div className="flex w-full h-12">
+                        <div className="w-2/12 h-full">
+                            <div className="h-12 w-12 bg-green-600 rounded-full flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="mx-auto text-white" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M2 9h19a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V9zm1-6h15v4H2V4a1 1 0 0 1 1-1zm12 11v2h3v-2h-3z" /></svg>
+                            </div>
+                        </div>
+                        <div className="w-6/12 h-full flex items-start">
+                            <p className="my-auto text-lg font-semibold">East park</p>
+                        </div>
+                        <div className="w-4/12 h-full flex justify-end items-end">
+                            <button className="flex bg-red-600 rounded-md my-auto px-5 py-1 float-right text-white font-medium">2 Trades</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div></>
     );
 }
 
