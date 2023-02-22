90f4ffa07530939aff00a9ce6a95d642790dd745
Harry Findlay <harryfindlay@outlook.com>
2023-02-22 19:56:28 +0000

    type: ignoring string indexing issues

--- backend/apps/api/tests/tests_authentication.py
+++ backend/apps/api/tests/tests_authentication.py
@@ -5,26 +5,26 @@ authentication.py test file!
 import ast
 
 import pytest
-from apps.api.authentication import gen_token
+from api.authentication import gen_token 
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.test import TestCase
 
 
 @pytest.mark.django_db
-def test_user_create():
+def test_user_create() -> None:
     User.objects.create_user("test_user", "test@test.com", "testpassword")
     assert User.objects.count() == 1
 
 
 @pytest.mark.django_db
-def test_user_authenticate():
+def test_user_authenticate() -> None:
     User.objects.create_user("test_user", "test@test.com", "testpassword")
     assert authenticate(username="test_user", password="testpassword") is not None
 
 
 @pytest.mark.django_db
-def test_user_not_authenticated():
+def test_user_not_authenticated() -> None:
     User.objects.create_user("test_user", "test@test.com", "testpassword")
     assert authenticate(username="test_user", password="WRONG_PASSWORD") is None
 
@@ -34,16 +34,16 @@ class RequestTests(TestCase):
     These tests run in an almost isolated database environment, they are never stored!
     """
 
-    def setUp(self):
+    def setUp(self) -> None:
         self.user = User.objects.create_user(
             username="test_harry", password="SUPER_SECURE"
         )
 
-    def test_gen_token(self):
+    def test_gen_token(self) -> None:
         test_token = gen_token(self.user)
         assert test_token is not None
 
-    def test_register(self):
+    def test_register(self) -> None:
         # send login data
         response = self.client.post(
             "/api/register",
@@ -59,9 +59,9 @@ class RequestTests(TestCase):
         response_dict = response.content.decode("UTF-8")
         response_dict = ast.literal_eval(response_dict)
 
-        self.assertEqual(response_dict["status"], "OK")
+        self.assertEqual(response_dict["status"], "OK") #type: ignore
 
-    def test_register_unqiue_error(self):
+    def test_register_unqiue_error(self) -> None:
         # send login data
         response = self.client.post(
             "/api/register",
@@ -78,9 +78,9 @@ class RequestTests(TestCase):
         response_dict = response.content.decode("UTF-8")
         response_dict = ast.literal_eval(response_dict)
 
-        self.assertEqual(response_dict["status"], "UNIQUE_ERROR")
+        self.assertEqual(response_dict["status"], "UNIQUE_ERROR") #type: ignore
 
-    def test_login(self):
+    def test_login(self) -> None:
         # send login data
         response = self.client.post(
             "/api/login",
@@ -92,9 +92,9 @@ class RequestTests(TestCase):
         response_dict = response.content.decode("UTF-8")
         response_dict = ast.literal_eval(response_dict)
 
-        self.assertEqual(response_dict["status"], "OK")
+        self.assertEqual(response_dict["status"], "OK") #type: ignore
 
-    def test_login_fail(self):
+    def test_login_fail(self) -> None:
         response = self.client.post(
             "/api/login",
             {"user": "test_harry", "password": "WRONG_PASSWORD"},
@@ -105,4 +105,4 @@ class RequestTests(TestCase):
         response_dict = response.content.decode("UTF-8")
         response_dict = ast.literal_eval(response_dict)
 
-        self.assertEqual(response_dict["status"], "BAD")
+        self.assertEqual(response_dict["status"], "BAD") #type: ignore
