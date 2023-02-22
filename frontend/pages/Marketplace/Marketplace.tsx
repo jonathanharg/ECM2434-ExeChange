dcba1ae6ef07a616a3ea0639b341a9bd95c27e04
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-02-22 00:28:31 +0000

    Fixed marketplace bug

--- frontend/pages/Marketplace/Marketplace.tsx
+++ frontend/pages/Marketplace/Marketplace.tsx
@@ -77,7 +77,7 @@ function Marketplace() {
 
           <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
         </div>
-        <h1>Hello {auth().user}</h1>
+        {/* <h1>Hello {auth().user}</h1> */}
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <Itemtile key={product.id} {...product} />
