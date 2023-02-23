5f9d61f32dddc786298dca420d7a86e3d13fdcdc
svyslf <saveyourselfh@gmail.com>
2023-02-23 17:38:10 +0000

    preliminary search (bugged)

--- frontend/pages/Marketplace/Marketplace.tsx
+++ frontend/pages/Marketplace/Marketplace.tsx
@@ -4,8 +4,8 @@ import { Product, tag } from "./Itemtile";
 import { useAuthUser } from "react-auth-kit";
 
 function Marketplace() {
-  const auth = useAuthUser();
-  const [searchState, setSearchState] = useState<tag>();
+  const [searchState, setSearchState] = useState<number>();
+  const [tags, setTags] = useState<tag[]>();
   const [products, setProducts] = useState<Product[]>([]);
   const fetchProducts = () => {
     return fetch("/api/products")
@@ -13,8 +13,15 @@ function Marketplace() {
       .then((data) => setProducts(data));
   };
 
+  const fetchTags = () => {
+    return fetch("/api/tags")
+      .then((response) => response.json())
+      .then((data) => setTags(data))
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchTags();
   }, []);
 
   return (
@@ -30,16 +37,15 @@ function Marketplace() {
           <select
             id="countries"
             className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-400 focus:border-green-800 focus:ring-green-800 dark:border-gray-600 dark:bg-gray-200 dark:text-green-900 dark:placeholder-gray-400 dark:focus:border-green-800 dark:focus:ring-green-800"
-            // onChange={(e) => setSearchState(e.target.value)}
+            onChange={(e) => setSearchState(tags.find((tag) => tag.id = parseInt(e.target.value)).id)}
           >
-            {/* {tagArray.map(t => (
+            {tags?.map(t => (
               <option key={t.id} value={t.id}>{t.name}</option>
-            ))} */}
+            ))}
           </select>
         </div>
-        {/* <h1>Hello {auth().user}</h1> */}
         <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-6">
-          {products.map((product) => (
+          {products.filter((product) => product.tags.map(t => t.id).includes(searchState)).map((product) => (
             <Itemtile key={product.id} {...product} />
           ))}
         </div>
