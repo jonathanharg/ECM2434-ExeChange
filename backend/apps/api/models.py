55e5ce89d42e362e94305b624bbe00ba5b80d797
Harry Findlay <harryfindlay@outlook.com>
2023-02-28 17:29:40 +0000

    updating models

--- backend/apps/api/models.py
+++ backend/apps/api/models.py
@@ -34,28 +34,28 @@ class ClothingItem(models.Model):
     def __str__(self) -> str:
         return self.caption
 
-class PendingTrade(models.Model):
-    """
-    Initiator: This is the person who has requested the trade.
+# class PendingTrade(models.Model):
+#     """
+#     Initiator: This is the person who has requested the trade.
 
-    Acceptor: The person who has accepted the trade.
-    """
-    initiator = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        default=None,
-    )
-    acceptor = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        default=None,
-    )
+#     Acceptor: The person who has accepted the trade.
+#     """
+#     initiator = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#         default=None,
+#     )
+#     acceptor = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#         default=None,
+#     )
 
-    initiator_items = models.ManyToManyField(ClothingItem, on_delete=models.CASCADE, default=None)
-    acceptor_items = models.ManyToManyField(ClothingItem, on_delete=models.CASCADE, default=None)
+#     initiator_items = models.ManyToManyField(ClothingItem, on_delete=models.CASCADE, default=None)
+#     acceptor_items = models.ManyToManyField(ClothingItem, on_delete=models.CASCADE, default=None)
 
-    initiator_verification = models.BooleanField(default=False)
-    acceptor_verification = models.BooleanField(default=False)
+#     initiator_verification = models.BooleanField(default=False)
+#     acceptor_verification = models.BooleanField(default=False)
 
 
 
