e1ed3454abd004da8bef37850028092420edacf8
Vihan <saveyourselfh@gmail.com>
2023-03-02 01:33:59 +0000

    trading popup visual overhaul

--- frontend/pages/Marketplace/Itemtile.tsx
+++ frontend/pages/Marketplace/Itemtile.tsx
@@ -55,7 +55,7 @@ export function Itemtile(product: Product) {
                 leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
               >
                 <Dialog.Panel className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
-                  <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <div className="relative flex w-full rounded-md items-center overflow-hidden bg-white px-4 pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
                     <button
                       type="button"
                       className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
--- frontend/pages/Marketplace/TradingPopup.tsx
+++ frontend/pages/Marketplace/TradingPopup.tsx
@@ -110,18 +110,23 @@ export function Trading(product: Product) {
 
   return (
   <div className="grid w-full grid-cols-1 items-start gap-y-8 gap-x-6 sm:grid-cols-12 lg:gap-x-8">
+    <div className="overflow-hidden rounded-lg sm:col-span-4 lg:col-span-5">
     <div className="aspect-w-2 aspect-h-3 mt-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
-      <img src={product.image} className="object-cover object-center"/>
+      <img src={product.image} className=" object-cover object-center"/>
     </div>
-
+    <h3 className="text-2xl mt-5 font-bold text-gray-900 sm:pr-12"> {product.caption} </h3>
+    <p className="mt-1 text-sm text-gray-500">
+      <b>Tagged by {product.owner.username} as: </b>{product.tags.map((t) => t.value).join(", ")}
+    </p>
+    </div>
     <div className="sm:col-span-8 lg:col-span-7">
-      <section aria-labelledby="options-heading" className="mt-10">
+      <section aria-labelledby="options-heading">
         <form method="POST" onSubmit={handleSubmit} className="">
           {/*Location drop down menu*/}
           <Listbox value={selectedLocation} onChange={setSelectedLocation}>
             {({ open }) => (
               <>
-                <Listbox.Label className="block text-sm font-medium text-gray-700 py-2">
+                <Listbox.Label className="block text-sm font-bold text-gray-700 py-2">
                   Select trade location
                 </Listbox.Label>
                 <div className="relative">
@@ -189,7 +194,7 @@ export function Trading(product: Product) {
           <Listbox value={selectedTime} onChange={setSelectedTime}>
             {({ open }) => (
               <>
-                <Listbox.Label className="block text-sm font-medium py-2 text-gray-700">
+                <Listbox.Label className="block text-sm font-bold py-2 text-gray-700 mt-[1rem]">
                   Select time
                 </Listbox.Label>
                 <div className="relative">
@@ -251,13 +256,13 @@ export function Trading(product: Product) {
               </>
             )}
           </Listbox>
-          <div className="sm:ml-0 lg:ml-7">
+          <div className="sm:ml-0 lg:ml-7 mt-[1.75rem]">
             <Calendar onChange={onDatesChange} value={selectedDates} minDate={new Date()}/>
           </div>
           <button
             type="submit"
-            className="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-[#17742b] py-3 px-8 text-base font-medium text-white hover:bg-[#17742b] focus:outline-none focus:ring-2 focus:ring-[#1d8d35] focus:ring-offset-2">
-            Request trade with {product.owner.username !== profileData?.name ? product.owner.username : "" }!
+            className="mt-[3rem] flex w-full items-center justify-center rounded-md border border-transparent bg-[#17742b] py-3 px-8 text-base font-bold text-white hover:bg-[#17742b] focus:outline-none focus:ring-2 focus:ring-[#1d8d35] focus:ring-offset-2">
+            Request trade with {product.owner.username}!
           </button>
         </form>
       </section>
