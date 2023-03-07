2ec742828847c850e9487009094e533795b099b0
Maddie Hansford <madelene.hansford@gmail.com>
2023-03-07 15:37:10 +0000

    started to separate achievements

---
+++ frontend/pages/Profile/Achievement.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export interface Reward {
+    text: string;
+    colour: string;
+}
+
+function Achievement(reward: Reward) {
+    const format = 
+    "font-ligth flex w-32 rounded-full " +reward.colour+ " px-4 py-2 text-white";
+  return (
+    <button className= {format} >
+        {reward.text}
+    </button>
+  );
+}
+
+export default Achievement;
