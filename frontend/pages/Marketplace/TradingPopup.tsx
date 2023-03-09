afcc57616f5ebef7cee31db3820bfced8d7f518e
Vihan <saveyourselfh@gmail.com>
2023-03-09 15:13:35 +0000

    profile stuff on trade popup

--- frontend/pages/Marketplace/TradingPopup.tsx
+++ frontend/pages/Marketplace/TradingPopup.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Transition } from "@headlessui/react";
 import { Listbox } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
@@ -7,13 +7,34 @@ import axios from "axios";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.xsrfCookieName = "csrftoken";
 import { Product } from "./Itemtile";
+import UserCircleIcon from "@heroicons/react/24/outline/UserCircleIcon";
+
+interface ProfileData {
+  levelPercent: number;
+  name: string;
+  level: number;
+}
+
+
+
 
 export function Trading(product: Product) {
   const [requestMessage, setRequestMessage] = useState("");
+  const [profileData, setProfileData] = useState<ProfileData>();
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
+
+  function fetchProfileData() {
+    return fetch("/api/profiledata")
+      .then((response) => response.json())
+      .then((data) => setProfileData(data));
+  }
+
+  useEffect(() => {
+    fetchProfileData();
+  }, []);
   const handleSubmit = async (e) => {
     // this function sends form data to /api/trade
     e.preventDefault();
@@ -62,17 +83,44 @@ export function Trading(product: Product) {
       </div>
       <div className="sm:col-span-8 lg:col-span-7">
         <section aria-labelledby="options-heading">
-
-
-          <div className="desc">  
-              {product.description}
+          <div className="flex items-center justify-between px-4 pt-12">
+            <UserCircleIcon className="h-16 w-16" />
+            <div className="flex w-9/12 items-center">
+              <div className="flex w-10/12 flex-col pl-4 leading-none">
+                <p className="text-sm font-bold">{product.owner.username}</p>
+                <p className="pt-1 text-sm font-light text-gray-800">
+                  Level {profileData?.level}
+                </p>
+                <div className="mb-1 text-base font-medium text-green-700 dark:text-green-500"></div>
+                <div className="mb-4 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+                  <div
+                    className="h-2.5 rounded-full bg-green-600 dark:bg-green-500"
+                    style={{ width: profileData?.levelPercent + "%" }}
+                  >
+                  </div>
+                </div>
+              </div>
+              <div className="w-2/12">
+                <div>
+                  {/* <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-700" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M9.243 19H21v2H3v-4.243l9.9-9.9 4.242 4.244L9.242 19zm5.07-13.556l2.122-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z" /></svg> */}
+                </div>
+              </div>
+            </div>
           </div>
-          <form method="POST" onSubmit={handleSubmit} className="">
 
-           <div className= "">
-           <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Write a message with your request!</label>
-            <textarea id="message" value = {requestMessage} onChange={(e)=> setRequestMessage(e.target.value)} className="row-span-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Here, you can let the trader know when you're free or discuss any other details about your request..."></textarea>
+       
 
+        <div className="mt-2 mb-2 absolute py-10"> 
+          <p className="text-sm text-gray-500">   
+              {product.description}  
+          </p> 
+        </div>
+
+          <form method="POST" onSubmit={handleSubmit} className="mt-[6rem]">
+
+           <div className="">
+            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Write a message with your request!</label>
+              <textarea id="message" value = {requestMessage} onChange={(e)=> setRequestMessage(e.target.value)} className="row-span-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Here, you can let the trader know when you're free or discuss any other details about your request..."></textarea>
            </div>
             <button
               type="submit"
