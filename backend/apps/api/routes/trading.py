0f7ce0574b31280703983e7e7a7c92869567a5b2
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-03-10 01:24:54 +0000

    Added time support

--- backend/apps/api/routes/trading.py
+++ backend/apps/api/routes/trading.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, timezone
+
 from apps.api.authentication import authenticate_user
 from apps.api.models import ClothingItem, ExeChangeUser, Location, Trade
 from apps.api.serializer import TradeSerializer
@@ -6,6 +8,7 @@ from django.db.models import Q
 from django.http import HttpRequest, JsonResponse
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404
+from django.utils.dateparse import parse_datetime
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import (
@@ -79,8 +82,10 @@ def reject_trade(request: HttpRequest, trade_id: int) -> Response:
 
     if (trade.giver != user) & (trade.receiver != user):
         return TRADE_NOT_FOUND
-    
-    if (trade.status == trade.TradeStatuses.COMPLETED) | (trade.status == trade.TradeStatuses.REJECTED):
+
+    if (trade.status == trade.TradeStatuses.COMPLETED) | (
+        trade.status == trade.TradeStatuses.REJECTED
+    ):
         return TRADE_NOT_FOUND
 
     if ("reject" not in request.data) | (request.data["reject"] != True):
@@ -107,7 +112,7 @@ def accept_trade(request: HttpRequest, trade_id: int) -> Response:
     # Trade must be accepted by the person who is giving the items
     if trade.giver != user:
         return TRADE_NOT_FOUND
-    
+
     if trade.status != Trade.TradeStatuses.PENDING:
         return TRADE_NOT_FOUND
 
@@ -132,19 +137,37 @@ def accept_trade(request: HttpRequest, trade_id: int) -> Response:
 
     if any(trade.receiver != item.owner for item in receiver_exchanging):
         return INVALID_REQUEST_ITEMS
-    
+
     try:
         location = Location.objects.get(name=data["location"])
     except Location.DoesNotExist:
         return INVALID_LOCATION
 
+    try:
+        time = parse_datetime(data["time"])
+    except ValueError:
+        return INVALID_TIME
+
+    now = datetime.now(timezone.utc)
+    TRADING_HOURS = [9, 10, 11, 12, 13, 14, 15, 16]
+
+    if (
+        (time is None)
+        | (time.minute != 30)
+        | (time.hour not in TRADING_HOURS)
+        | (time <= now)
+        | ((time - now) > timedelta(days=7))
+        | time.weekday > 4 # Greater than Friday (4)
+    ):
+        return INVALID_TIME
+
+    time = time.replace(second=0, microsecond=0)  # ignore seconds
+
     trade.receiver_exchanging.set(receiver_exchanging)
     trade.location = location
     trade.status = trade.TradeStatuses.ACCEPTED
+    trade.time = time
     trade.save()
-    # trade.time
-    # trade.location
-
 
     return Response(
         {"status": "OK", "message": "Submission accepted", "id": trade.pk},
@@ -227,5 +250,10 @@ TRADE_NOT_FOUND = Response(
 
 INVALID_LOCATION = Response(
     {"status": "INVALID_LOCATION", "message": "Could not find the location."},
+    status=HTTP_400_BAD_REQUEST,
+)
+
+INVALID_TIME = Response(
+    {"status": "INVALID_TIME", "message": "The time is not valid."},
     status=HTTP_400_BAD_REQUEST,
 )
