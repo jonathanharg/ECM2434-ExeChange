5d64c8ba005563b7bbb4a7a5ca3e138b9616489c
Vihan <saveyourselfh@gmail.com>
2023-03-12 01:59:59 +0000

    Pagination #2 for trade requests

--- frontend/pages/TradeCenter/TradeResponse.tsx
+++ frontend/pages/TradeCenter/TradeResponse.tsx
@@ -1,20 +1,49 @@
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftIcon, ArrowRightIcon, CheckCircleIcon, TrashIcon } from "@heroicons/react/24/outline";
 import React, { useEffect, useState } from "react";
-
+import { Product } from "../Marketplace/Itemtile";
 import { TradeInvolvement } from "./TradeCenter";
 
 export default function TradeResponse(trade: TradeInvolvement) {
     const [page, setPage] = useState<number>(0)
+    const [products, setProducts] = useState<Product[]>([]);
+    const [reciever_exchanging, setRecieverExchanging] = useState<Number[]>()
 
-    function handleclick() {
-        setPage(1)
+    function fetchProducts() {
+        return fetch("/api/products")
+        .then((response) => response.json())
+        .then((data) => setProducts(data));
     }
+    useEffect(()=> {fetchProducts()}, [])
+    function handleclick() {
+        console.log(products.map((i)=> i.owner.id))
+        setPage(1)
+        console.log(page)
+    }
+    
+    function handleExtraItems(index) {
+        if(reciever_exchanging){
+            if (reciever_exchanging.includes(index)) {
+
+            setRecieverExchanging(reciever_exchanging.filter((i) => i !== index));
+            } else {
+            setRecieverExchanging([...reciever_exchanging, index]);
+            }
+        }
+      }
+
+    function showSvg(index) {
+        if(reciever_exchanging){
+            if(reciever_exchanging.includes(index)) {    
+            return <CheckCircleIcon className={`w-6 h-6 absolute right-0 stroke-green-800`}> </CheckCircleIcon>
+            } 
+        }
+      }
     return (
         <div className="w-full"> 
-            <div className="p-4 overflow-hidden flex flex-col items-center rounded-lg">
+            <div className={page == 0 ? "p-4 overflow-hidden flex flex-col items-center rounded-lg": "hidden"}>
                 {trade.giver_giving.length == 1 ? 
                   <>
-                    <h3 className="text-xl font-bold text-gray-900">
+                    <h3 className="text-xl p-5 font-bold text-gray-900">
                     {" "}
                     {trade.receiver.username} wants the {trade.giver_giving.map((i) => i.caption)}{" "}
                     </h3> 
@@ -36,7 +65,7 @@ export default function TradeResponse(trade: TradeInvolvement) {
                 </>
                 : 
                 <>
-                    <h3 className="text-xl font-bold text-gray-900">
+                    <h3 className="text-xl p-5 font-bold text-gray-900">
                     {" "}
                     {trade.receiver.username} wants the following items: {" "}
                     </h3>
@@ -55,12 +84,67 @@ export default function TradeResponse(trade: TradeInvolvement) {
                     </p> 
                 </>
                 }  
-                </div>
-                <div className="pb-4 flex justify-center items-center">
-                    <button onClick ={()=> handleclick} className="w-fit flex items-center p-2 text-sm font-medium text-white bg-green-800 border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700">
-                        Pick the Items you would like in exechange 
-                        <ArrowRightIcon className="h-3 w-3"></ArrowRightIcon>
+                <div className="p-2 flex justify-center items-center">
+                    <button onClick ={handleclick} className="w-fit flex items-center p-2 text-sm font-medium text-white bg-green-800 border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700">
+                        Pick the Items you would like in exechange
+                        <ArrowRightIcon className=" m-2 h-3 w-3"></ArrowRightIcon>
                     </button>
+                </div>
+            </div>
+            <div className={page == 1 ? "p-4 overflow-hidden flex flex-col items-center rounded-lg": "hidden"}>
+
+                {products.filter((i)=> i.owner.id == trade.receiver.id).length > 0 ? 
+                  <>
+                    <h3 className="text-xl p-5 font-bold text-gray-900">
+                    {" "}
+                    {trade.receiver.username} has the following item(s):{" "}
+                    </h3> 
+                    <div className="p-4 container grid grid-flow-dense grid-cols-4 gap-2 rounded-md overflow-hidden mx-auto py-1"> 
+                        {products.filter(i=>i.owner.id==trade.receiver.id).map((i) =>  
+                        <div key= {i.id} className="w-full relative overflow-hidden rounded-md bg-gray-200 hover:opacity-75">
+                        {showSvg(i.id)}
+                        <img
+                        draggable={false}
+                        tabIndex={1}
+                        src={i.image}
+                        onClick={() => handleExtraItems(i.id) }
+                        />
+                        </div>)}       
+                    </div>
+                    <p className="text-sm pt-2 text-gray-500">   
+                        <b> Message from {trade.receiver.username}: </b> {trade.message}  
+                    </p> 
+                </>
+                : 
+                <>
+                    <div className="flex justify-center items-center">
+                        <h3 className="text-md font-bold text-gray-900">
+                        {" "}
+                            Looks like {trade.receiver.username} hasn't posted any items yet! 
+                        {" "}
+                        </h3>
+                    </div>
+                    <p className="text-sm pt-2 text-gray-500">   
+                            <b> psst... </b> You can still give away your item
+                    </p> 
+                </>
+                }  
+
+                <div className="p-2 w-full grid grid-cols-3 ">
+                    <div className="justify-left">
+                        <button onClick ={handleclick} className="w-fit flex items-center p-2 text-sm font-medium text-white bg-red-800 border border-gray-300 rounded-lg hover:bg-red-700 hover:text-gray-700">
+                            Decline
+                            <TrashIcon className=" stroke-2 m-2 h-3 w-3"></TrashIcon>
+                        </button>
+                    </div>
+                    <div className="justify-center"> </div>
+                    <div className="justify-right">
+                        <button onClick ={handleclick} className="w-fit flex items-center p-2 text-sm font-medium text-white bg-green-800 border border-gray-300 rounded-lg hover:bg-green-700 hover:text-gray-700">
+                            Next
+                            <ArrowRightIcon className="stroke-2 m-2 h-3 w-3"></ArrowRightIcon>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>  
     )
