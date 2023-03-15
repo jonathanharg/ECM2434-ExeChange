4b4182e3e8f58e010417accc6894657a77f6c6b2
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-03-15 01:24:38 +0000

    Added basic support for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import { splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   test: {
@@ -10,7 +11,36 @@ export default defineConfig({
       reporter: ["text"],
     },
   },
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    VitePWA({
+      // strategies: "injectManifest",
+      registerType: "autoUpdate",
+      injectRegister: "script",
+      manifest: {
+        name: 'ExeChange',
+        short_name: 'ExeChange',
+        start_url: '/',
+        scope: '/',
+        description: 'Swap your your clothes on campus!',
+        theme_color: '#166534',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/static/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/static/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
   build: { manifest: true },
   base: "/static/",
   root: "./frontend",
