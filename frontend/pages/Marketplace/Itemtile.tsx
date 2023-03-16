6a83bfb9eeccdd36eeb4aded15e66942abd7a648
augustijn <av407@exeter.ac.uk>
2023-03-16 17:11:59 +0000

    frontend delete item

--- frontend/pages/Marketplace/Itemtile.tsx
+++ frontend/pages/Marketplace/Itemtile.tsx
@@ -3,6 +3,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Trading } from "./TradingPopup";
 import { useAuthUser } from "react-auth-kit";
+import { DeleteItem } from "../Profile/DeleteItem";
 
 export interface Product {
   id: number;
@@ -24,8 +25,8 @@ export type tag = {
   label: string;
 };
 
-export function Itemtile(product: Product, Function) {
-  /* , {trading}:boolean */
+export function Itemtile(product: Product) { 
+
   const [open, setOpen] = useState(false);
   const auth = useAuthUser();
   return (
@@ -66,7 +67,7 @@ export function Itemtile(product: Product, Function) {
                     </button>
                     {/* if(trading) {  } */}
                     {/* ADD IF CONDITION HERE - IF TRADING then : otherwise idk not that  */}
-                    <Function/>
+                    <Trading key={product.id} {...product}/>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
@@ -87,7 +88,7 @@ export function Itemtile(product: Product, Function) {
           onClick={() => setOpen(true)}
         />
       </div>
-      <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+      <DeleteItem />
       <div className="mt-2 flex justify-between">
         <div>
           <h3 className="text-sm text-gray-700">
---
+++ frontend/pages/Profile/DeleteItem.tsx
@@ -0,0 +1,120 @@
+import React, { Fragment, useRef, useState } from "react";
+import {Dialog, Transition } from "@headlessui/react";
+import axios from "axios";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+axios.defaults.xsrfCookieName = "csrftoken";
+
+export function DeleteItem() {
+  const [open, setOpen] = useState(false)
+  const [checkedDelete, setCheckedDelete] = useState(false);
+  const cancelButtonRef = useRef(null)
+
+  function clickHandler() {
+    setOpen(false);
+    setCheckedDelete(true);
+  }
+
+  const handleSubmit = async (e) => {
+    // this function sends form data to /api/trade
+    e.preventDefault();
+
+    await axios
+      .post(
+        "/api/deleteitem",
+        JSON.stringify({
+            checkedDelete,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        // TODO: Handle more responses than just OK
+        if (response.data.status != "OK") {
+          return;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+    <form method="POST" onSubmit={handleSubmit} className="">
+        <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+        >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                        Delete Item
+                        </Dialog.Title>
+                        <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                            Are you sure you want to delete your item?
+                        </p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                    type="submit"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    onClick={() => clickHandler()}>
+                    Delete
+                    </button>
+                    <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}>
+                    Cancel
+                    </button>
+                </div>
+                </Dialog.Panel>
+            </Transition.Child>
+            </div>
+        </div>
+        </Dialog>
+        </Transition.Root>
+        <button
+            type="button"
+            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+            onClick={() => setOpen(true)}
+            ref={cancelButtonRef}
+            >
+            Delete Item
+        </button>
+    </form>
+    </>
+  );
+}
+
+export default DeleteItem;
