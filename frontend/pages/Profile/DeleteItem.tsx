200db7209cdcae0196effc888d16946d3b4db75a
augustijn <av407@exeter.ac.uk>
2023-03-20 16:16:00 +0000

    removed form

--- frontend/pages/Profile/DeleteItem.tsx
+++ frontend/pages/Profile/DeleteItem.tsx
@@ -40,8 +40,7 @@ export function DeleteItem(product: Product) {
 
   return (
     <>
-    <form method="POST" onSubmit={handleSubmit} className="">
-        <Transition.Root show={open} as={Fragment}>
+      <Transition.Root show={open} as={Fragment}>
         <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
         <Transition.Child
             as={Fragment}
@@ -83,7 +82,7 @@ export function DeleteItem(product: Product) {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                     <button
-                    type="submit"
+                    type="button"
                     className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                     onClick={(e) => handleSubmit(e)}>
                     Delete
@@ -101,17 +100,15 @@ export function DeleteItem(product: Product) {
             </div>
         </div>
         </Dialog>
-        </Transition.Root>
-        <button
-            type="button"
-            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-            onClick={() => setOpen(true)}
-            ref={cancelButtonRef}
-            >
-            <TrashIcon className="h-5 w-5"
-                                      aria-hidden="true"/>
-        </button>
-    </form>
+      </Transition.Root>
+      <button
+          type="button"
+          className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+          onClick={() => setOpen(true)}
+          ref={cancelButtonRef}
+          >
+          <TrashIcon className="h-5 w-5" aria-hidden="true"/>
+      </button>
     </>
   );
 }
