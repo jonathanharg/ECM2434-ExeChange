02729eb72c79ba64ed0232d08c0582e1a91ff4c6
gabby <gabbyclifton.gc@gmail.com>
2023-03-21 17:15:27 +0000

    added locations

--- frontend/pages/Profile/Profile.tsx
+++ frontend/pages/Profile/Profile.tsx
@@ -1,23 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {
-  ChatBubbleLeftRightIcon,
-  PowerIcon,
-  CameraIcon,
-  UserGroupIcon,
-  BookOpenIcon,
-  CloudIcon,
-  DocumentChartBarIcon,
-  BuildingLibraryIcon,
-  RocketLaunchIcon,
-  NewspaperIcon,
-  BuildingOffice2Icon,
-  FilmIcon,
-  MoonIcon,
-  BeakerIcon,
-  LightBulbIcon,
-  TicketIcon,
-} from "@heroicons/react/24/outline";
+
 import Tradealert from "./Tradealert";
 import { Location } from "./Badge";
 import Badge from "./Badge";
@@ -28,121 +11,9 @@ import { achievement } from "./Achievement";
 import Profilestats from "./Profilestats";
 import { ProfileData } from "./Profilestats"
 import { useAuthUser, useIsAuthenticated } from "react-auth-kit";
+import { Locations } from "./Locations";
 
-const locations: Location[] = [
-  {
-    id: 1,
-    colour: "bg-yellow-400",
-    place: "Lafrowda",
-    icon: UserGroupIcon,
-    trades: 3,
-  },
-  {
-    id: 2,
-    colour: "bg-blue-500",
-    place: "Library",
-    icon: BookOpenIcon,
-    trades: 4,
-  },
-  {
-    id: 3,
-    colour: "bg-orange-600",
-    place: "Holland Hall",
-    icon: CameraIcon,
-    trades: 3,
-  },
-  {
-    id: 4,
-    colour: "bg-purple-500",
-    place: "Sports Park",
-    icon: PowerIcon,
-    trades: 3,
-  },
-  {
-    id: 5,
-    colour: "bg-green-900",
-    place: "Forum",
-    icon: ChatBubbleLeftRightIcon,
-    trades: 2,
-  },
-  {
-    id: 6,
-    colour: "bg-pink-600",
-    place: "Mardon",
-    icon: CloudIcon,
-    trades: 3,
-  },
-  {
-    id: 7,
-    colour: "bg-blue-700",
-    place: "Peter Chalk",
-    icon: DocumentChartBarIcon,
-    trades: 3,
-  },
-  {
-    id: 8,
-    colour: "bg-gray-600",
-    place: "Reed Hall",
-    icon: BuildingLibraryIcon,
-    trades: 2,
-  },
-  {
-    id: 9,
-    colour: "bg-red-500",
-    place: "Physics Building",
-    icon: RocketLaunchIcon,
-    trades: 4,
-  },
-  {
-    id: 10,
-    colour: "bg-teal-600",
-    place: "Queen's",
-    icon: NewspaperIcon,
-    trades: 3,
-  },
-  {
-    id: 11,
-    colour: "bg-teal-900",
-    place: "Washington Singer",
-    icon: MoonIcon,
-    trades: 3,
-  },
-  {
-    id: 12,
-    colour: "bg-orange-800",
-    place: "Old Library",
-    icon: FilmIcon,
-    trades: 5,
-  },
-  {
-    id: 13,
-    colour: "bg-pink-700",
-    place: "Amory",
-    icon: BeakerIcon,
-    trades: 3,
-  },
-  {
-    id: 14,
-    colour: "bg-yellow-600",
-    place: "Innovation Centre",
-    icon: LightBulbIcon,
-    trades: 4,
-  },
-  {
-    id: 15,
-    colour: "bg-purple-800",
-    place: "Northcott Theatre",
-    icon: TicketIcon,
-    trades: 3,
-  },
-  {
-    id: 16,
-    colour: "bg-green-400",
-    place: "East Park",
-    icon: BuildingOffice2Icon,
-    trades: 3,
-  },
-];
+
 
 
 function Profile() {
@@ -189,9 +60,7 @@ function Profile() {
 
       <div className="flex w-full flex-col px-4 pt-12">
         <p className="font-semibold text-gray-600">Location Badges</p>
-        {locations.map((location) => (
-          <Badge key={location.id} {...location} />
-        ))}
+        <Locations key={profileData?.id}{...profileData}/>
       </div>
       <div className="flex w-full flex-col px-4 pt-12">
         <MyItems/>
