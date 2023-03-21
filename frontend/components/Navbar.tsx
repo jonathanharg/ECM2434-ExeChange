35aa06d31b160f464c07efa022d051d1ae088f68
Harry Findlay <harryfindlay@outlook.com>
2023-03-21 11:34:25 +0000

    frontend notification basics

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -238,31 +238,29 @@ export default function Navbar() {
                   {/* Notifications */}
                   <div className="ml-4 flow-root lg:ml-6">
                     <Popover className="relative">
-                        {() => (
-                          <>
-                            <Popover.Button>
-                              <BellIcon
-                                className="h-6 w-6 text-gray-400 hover:text-gray-500"
-                                aria-hidden="true"
-                              />
-                            </Popover.Button>
-                            <Transition
-                              as={Fragment}
-                              enter="transition ease-out duration-200"
-                              enterFrom="opacity-0 translate-y-1"
-                              enterTo="opacity-100 translate-y-0"
-                              leave="transition ease-in duration-150"
-                              leaveFrom="opacity-100 translate-y-0"
-                              leaveTo="opacity-0 translate-y-1"
-                            >
-                              <Popover.Panel
-                              className="w-screen min-w-max sm:mr-5 sm:max-w-xs sm:shadow-xl"
-                              >
-                                <NotificationBox />
-                              </Popover.Panel>
-                            </Transition>
-                          </>
-                        )}
+                      {() => (
+                        <>
+                          <Popover.Button>
+                            <BellIcon
+                              className="h-6 w-6 text-gray-400 hover:text-gray-500"
+                              aria-hidden="true"
+                            />
+                          </Popover.Button>
+                          <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0 translate-y-1"
+                            enterTo="opacity-100 translate-y-0"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100 translate-y-0"
+                            leaveTo="opacity-0 translate-y-1"
+                          >
+                            <Popover.Panel className="w-screen min-w-max sm:mr-5 sm:max-w-xs sm:shadow-xl">
+                              <NotificationBox />
+                            </Popover.Panel>
+                          </Transition>
+                        </>
+                      )}
                     </Popover>
                   </div>
 
--- frontend/components/NotificationBox.tsx
+++ frontend/components/NotificationBox.tsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface Notification {
+  id: number;
+  notification_type: string,
+  text: string;
+  link: string;
+}
+
+function SingleNotification(notification: Notification) {
+    return (
+        <div>
+            <p><strong>{notification.notification_type}</strong>: {notification.text}</p>
+        </div>
+    )
+}
 
 function NotificationBox() {
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
+  function getNotifications() {
+    return fetch("/api/getnotifications")
+      .then((response) => response.json())
+      .then((data) => setNotifications(data.notifications));
+  }
+
+  useEffect(() => {
+    getNotifications();
+  });
+
   return (
     <div>
-    <h1>HELLO</h1>
-    <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-    </ul>
+        <ul>
+            {
+                notifications.map((notification) => (
+                    <SingleNotification key={notification.id} {...notification}/>
+                ))
+            }
+        </ul>
     </div>
-  )
+  );
 }
 export default NotificationBox;
