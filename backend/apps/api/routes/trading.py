26caeed312b0fad3213130238113d958cdba6398
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-03-22 15:16:12 +0000

    Rejected trades no longer show

--- backend/apps/api/routes/trading.py
+++ backend/apps/api/routes/trading.py
@@ -373,10 +373,10 @@ def get_trades(request: HttpRequest) -> Response:
     active_trades = trades.exclude(
         Q(status=Trade.TradeStatuses.ACCEPTED) | Q(status=Trade.TradeStatuses.REJECTED)
     ).order_by("requested_at")
-    rejected_trades = trades.filter(status=Trade.TradeStatuses.REJECTED).order_by(
-        "requested_at"
-    )
+    # rejected_trades = trades.filter(status=Trade.TradeStatuses.REJECTED).order_by(
+    #     "requested_at"
+    # )
     trades_serializer = TradeSerializer(
-        list(chain(accepted_trades, active_trades, rejected_trades)), many=True
+        list(chain(accepted_trades, active_trades)), many=True
     )
     return JsonResponse(trades_serializer.data, safe=False)
